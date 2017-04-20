6c0238cb63b9f2a145dbd46d6d843c8e18fbdc1c
zhanghui <zhanghui@ablecloud.cn>
2017-04-20 11:40:41 +0800

    add upload

---
+++ robots/mqtt-robot/upload/upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ../robot.so ~/ablecloud.cn/src/deploys/ac-bench/roles/upload-mqtt-robot/files/
+cp ../schedulers.json ~/ablecloud.cn/src/deploys/ac-bench/roles/upload-mqtt-robot/files/
+cp ./robot.json ~/ablecloud.cn/src/deploys/ac-bench/roles/upload-mqtt-robot/templates/robot.json
+
+cd ~/ablecloud.cn/src/deploys/ac-bench/; ./upload-mqtt-robot.sh
