bf7dc059b45a3e1daed2986bcf58088102ac8c2a
zhanghui <zhanghui@ablecloud.cn>
2017-04-20 11:34:54 +0800

    update mqtt robot

--- robots/mqtt-robot/robot.go
+++ robots/mqtt-robot/robot.go
@@ -4,33 +4,56 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/eclipse/paho.mqtt.golang"
+	"github.com/ironzhang/golang/jsoncfg"
 	"github.com/ironzhang/toy/framework/robot"
+	"github.com/sirupsen/logrus"
 )
 
 var errTimeout = errors.New("timeout")
 
 var (
+	addr    = "tcp://localhost:1883"
 	timeout = 5 * time.Second
-	payload = strings.Repeat("A", 100)
+	payload = strings.Repeat("0", 50)
 )
 
+type Options struct {
+	Addr        string
+	Timeout     string
+	PayloadSize int
+	Start       int
+}
+
 func NewRobots(n int, file string) ([]robot.Robot, error) {
+	var opts Options
+	err := jsoncfg.LoadFromFile(file, &opts)
+	if err != nil {
+		return nil, err
+	}
+
+	addr = opts.Addr
+	timeout, err = time.ParseDuration(opts.Timeout)
+	if err != nil {
+		return nil, err
+	}
+	payload = strings.Repeat("0", opts.PayloadSize)
+
 	robots := make([]robot.Robot, 0, n)
-	for i := 0; i < n; i++ {
-		robots = append(robots, &Robot{addr: "tcp://localhost:1883", id: i + 1, ok: true})
+	for i := 1; i <= n; i++ {
+		robots = append(robots, &Robot{ok: true, id: opts.Start + i})
 	}
 	return robots, nil
 }
 
 type Robot struct {
-	addr string
-	id   int
-
 	ok bool
 	c  mqtt.Client
+
+	id int
 }
 
 func (r *Robot) OK() bool {
@@ -96,16 +119,27 @@ func (r *Robot) Disconnect() error {
 
 func (r *Robot) MqttClientOptions() *mqtt.ClientOptions {
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker(r.addr)
+	opts.AddBroker(addr)
 	opts.AutoReconnect = false
 	opts.DefaultPublishHander = r.OnMessage
 	opts.OnConnectionLost = r.OnConnectionLost
 	return opts
 }
 
+var msgcnt int64
+
 func (r *Robot) OnMessage(c mqtt.Client, msg mqtt.Message) {
+	n := atomic.AddInt64(&msgcnt, 1)
+	if n%100000 == 0 {
+		logrus.WithField("msgcnt", n).Info("on message")
+	}
 }
+
+var lost int64
 
 func (r *Robot) OnConnectionLost(c mqtt.Client, err error) {
 	r.ok = false
+
+	n := atomic.AddInt64(&lost, 1)
+	logrus.WithError(err).WithField("lost", n).Errorf("robot(%s) lost connection", r.id)
 }
