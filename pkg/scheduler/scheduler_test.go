6915b3e47b80241e75015e52e80dc5ffb9a05b11
zhanghui <zhanghui@ablecloud.cn>
2017-07-05 09:39:08 +0800

    add scheduler pkg

---
+++ pkg/scheduler/scheduler_test.go
@@ -0,0 +1,121 @@
+package scheduler
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/ironzhang/toy/framework/robot"
+)
+
+type SchedulerRobot struct {
+	ok    bool
+	count int64
+}
+
+func (r *SchedulerRobot) OK() bool {
+	return r.ok
+}
+
+func (r *SchedulerRobot) Do(name string) error {
+	atomic.AddInt64(&r.count, 1)
+	return nil
+}
+
+func TestSchedulerN(t *testing.T) {
+	r1 := &SchedulerRobot{ok: true}
+	r2 := &SchedulerRobot{ok: true}
+	robots := []robot.Robot{r1, r2}
+	e := Scheduler{
+		N: 1000,
+		C: 10,
+	}
+	e.Run(context.Background(), robots, nil)
+
+	if int(r1.count) != e.N {
+		t.Errorf("count(%d) != %d", r1.count, e.N)
+	}
+	if int(r2.count) != e.N {
+		t.Errorf("count(%d) != %d", r2.count, e.N)
+	}
+}
+
+func TestSchedulerQPS1(t *testing.T) {
+	r := &SchedulerRobot{ok: true}
+	robots := []robot.Robot{r}
+
+	start := time.Now()
+	tick := time.Tick(time.Second)
+	go func() {
+		for {
+			tt := <-tick
+			sec := int64(tt.Sub(start) / time.Second)
+			if r.count > sec {
+				t.Errorf("count(%d) > sec(%d)", r.count, sec)
+			}
+		}
+	}()
+
+	e := Scheduler{
+		N:   2,
+		C:   2,
+		QPS: 1,
+	}
+	e.Run(context.Background(), robots, nil)
+}
+
+func TestSchedulerQPS2(t *testing.T) {
+	r := &SchedulerRobot{ok: true}
+	robots := []robot.Robot{r}
+
+	(&Scheduler{
+		N:   10000,
+		C:   1,
+		QPS: 0,
+		//PrintReport: true,
+	}).Run(context.Background(), robots, nil)
+
+	(&Scheduler{
+		N:   10000,
+		C:   1,
+		QPS: 1000000,
+		//PrintReport: true,
+	}).Run(context.Background(), robots, nil)
+}
+
+func TestSchedulerDisplay(t *testing.T) {
+	r1 := &SchedulerRobot{ok: true}
+	robots := []robot.Robot{r1}
+
+	e := Scheduler{
+		N:       -1,
+		C:       2,
+		QPS:     10,
+		Name:    "TestSchedulerDisplay",
+		Display: true,
+		//PrintReport: true,
+	}
+	ctx, _ := context.WithDeadline(context.Background(), time.Now().Add(4*time.Second+300*time.Millisecond))
+	e.Run(ctx, robots, nil)
+}
+
+func TestSchedulerRobotOK(t *testing.T) {
+	r1 := &SchedulerRobot{ok: true}
+	r2 := &SchedulerRobot{ok: false}
+	robots := []robot.Robot{r1, r2}
+
+	e := Scheduler{
+		N: 5,
+		C: 2,
+		//PrintReport: true,
+	}
+	e.Run(context.Background(), robots, nil)
+
+	if r1.count != 5 {
+		t.Errorf("c1(%d) != 3", r1.count)
+	}
+	if r2.count != 0 {
+		t.Errorf("c2(%d) != 0", r2.count)
+	}
+}
---
+++ pkg/scheduler/work_test.go
@@ -0,0 +1,107 @@
+package scheduler
+
+import (
+	"fmt"
+	"sync/atomic"
+	"testing"
+
+	"github.com/ironzhang/toy/framework/robot"
+)
+
+type WorkRobot struct {
+	ConnectCount    int64
+	PrepareCount    int64
+	PublishCount    int64
+	DisconnectCount int64
+}
+
+func (r *WorkRobot) OK() bool {
+	return true
+}
+
+func (r *WorkRobot) Do(name string) error {
+	switch name {
+	case "Connect":
+		atomic.AddInt64(&r.ConnectCount, 1)
+	case "Prepare":
+		atomic.AddInt64(&r.PrepareCount, 1)
+	case "Publish":
+		atomic.AddInt64(&r.PublishCount, 1)
+	case "Disconnect":
+		atomic.AddInt64(&r.DisconnectCount, 1)
+	}
+	return nil
+}
+
+func TestWork(t *testing.T) {
+	n := 100
+	robots := make([]robot.Robot, 0, n)
+	for i := 0; i < n; i++ {
+		robots = append(robots, &WorkRobot{})
+	}
+
+	w := Work{
+		Ask:    false,
+		Robots: robots,
+		Schedulers: []Scheduler{
+			{
+				N:           1,
+				C:           10,
+				QPS:         1000,
+				Name:        "Connect",
+				Display:     false,
+				PrintReport: false,
+			},
+			{
+				N:           1,
+				C:           10,
+				QPS:         1000,
+				Name:        "Prepare",
+				Display:     false,
+				PrintReport: false,
+			},
+			{
+				N:      100,
+				C:      100,
+				QPS:    5000,
+				Name:   "Publish",
+				Sample: 100,
+				//Display:     true,
+				//PrintReport: true,
+			},
+			{
+				N:           1,
+				C:           10,
+				QPS:         1000,
+				Name:        "Disconnect",
+				Display:     false,
+				PrintReport: false,
+			},
+		},
+	}
+	w.Run()
+
+	for _, r := range robots {
+		wr := r.(*WorkRobot)
+		if wr.ConnectCount != 1 {
+			t.Errorf("ConnectCount: %d != 1", wr.ConnectCount)
+		}
+		if wr.PrepareCount != 1 {
+			t.Errorf("PrepareCount: %d != 1", wr.PrepareCount)
+		}
+		if wr.PublishCount != 100 {
+			t.Errorf("PublishCount: %d != 100", wr.PublishCount)
+		}
+		if wr.DisconnectCount != 1 {
+			t.Errorf("DisconnectCount: %d != 1", wr.DisconnectCount)
+		}
+	}
+}
+
+func ExampleAsk() {
+	if ask("TestAsk") {
+		fmt.Println("yes")
+	} else {
+		fmt.Println("no")
+	}
+}
