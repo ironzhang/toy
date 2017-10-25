5567ce041a3ae68f6101fc2bd8eb76b6eeac0474
zhanghui <zhanghui@ablecloud.cn>
2017-10-25 11:06:42 +0800

    update coap-robot

--- robots/coap-robot/robot.go
+++ robots/coap-robot/robot.go
@@ -11,32 +11,68 @@ import (
 var client coap.Client
 
 func init() {
-	coap.Verbose = 2
+	coap.Verbose = 0
 }
 
 func NewRobots(n int, file string) ([]robot.Robot, error) {
 	robots := make([]robot.Robot, 0, n)
 	for i := 1; i <= n; i++ {
-		robots = append(robots, &Robot{addr: "localhost:5683"})
+		robots = append(robots, &Robot{ok: true, addr: "localhost:5683"})
 	}
 	return robots, nil
 }
 
 type Robot struct {
+	ok   bool
 	addr string
+	conn *coap.Conn
 }
 
 func (r *Robot) OK() bool {
-	return true
+	return r.ok
 }
 
 func (r *Robot) Do(name string) error {
 	switch name {
+	case "Connect":
+		return r.Connect()
+	case "Disconnect":
+		return r.Disconnect()
 	case "Ping":
 		return r.Ping()
+	case "ShortPing":
+		return r.ShortPing()
 	default:
 		return fmt.Errorf("unknown task(%s)", name)
 	}
+}
+
+func (r *Robot) ShortPing() error {
+	urlstr := fmt.Sprintf("coap://%s/ping", r.addr)
+	req, err := coap.NewRequest(true, coap.POST, urlstr, nil)
+	if err != nil {
+		return err
+	}
+	_, err = client.SendRequest(req)
+	if err != nil {
+		log.Printf("send request: %v", err)
+		return err
+	}
+	return nil
+}
+
+func (r *Robot) Connect() (err error) {
+	urlstr := fmt.Sprintf("coap://%s", r.addr)
+	r.conn, err = client.Dial(urlstr, nil, nil)
+	if err != nil {
+		r.ok = false
+		return err
+	}
+	return nil
+}
+
+func (r *Robot) Disconnect() error {
+	return r.conn.Close()
 }
 
 func (r *Robot) Ping() error {
@@ -45,7 +81,7 @@ func (r *Robot) Ping() error {
 	if err != nil {
 		return err
 	}
-	_, err = client.SendRequest(req)
+	_, err = r.conn.SendRequest(req)
 	if err != nil {
 		log.Printf("send request: %v", err)
 		return err
--- robots/coap-robot/robot_test.go
+++ robots/coap-robot/robot_test.go
@@ -9,8 +9,8 @@ func TestRobot(t *testing.T) {
 	n := 1000
 	for i := 0; i < n; i++ {
 		r := Robot{addr: "localhost:5683"}
-		if err := r.Do("Ping"); err != nil {
-			t.Errorf("do ping: %v", err)
+		if err := r.Do("ShortPing"); err != nil {
+			t.Errorf("do short ping: %v", err)
 		}
 		log.Printf("robot(%d) done", i)
 	}
